199f85b4299ee095bc9c9331bc1dd78b65877a54
Shivam Prajapati <shivamsprajapati13@gmail.com>
2025-07-01 23:04:00 +0530

    Added env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CurDataModule } from './module/syncModule';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-  TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'demo',
-      database: 'salesdb',
-      autoLoadEntities: true,
-      synchronize: true, // OK for dev only
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-    CurDataModule, 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT'), 10),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // OK for development only
+      }),
+    }),
+
+    CurDataModule,
   ],
   controllers: [AppController],
   providers: [AppService],
